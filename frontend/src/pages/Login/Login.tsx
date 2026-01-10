58039fe1297f67f6d9653d6c5d4aa464b6c6474a
gchris2024 <gchris3586@gmail.com>
2026-01-09 22:39:02 -0500

    login first iteration

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import styles from "./Login.module.css";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -9,9 +10,22 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <p>The Home Page of All Time</p>
-      <button onClick={handleLogin}>Go to Dashboard</button>
+    <div className={styles.loginPage}>
+      <section className={styles.loginLeft}>
+        <h1>Sprintly</h1>
+        <p>Declutter your life.</p>
+      </section>
+
+      <section className={styles.loginRight}>
+        <form className={styles.loginForm}>
+          <h2>The Welcome Message</h2>
+
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button onClick={handleLogin}>Login</button>
+          <button type="button" className={styles.signUp}>New User? Sign Up</button>
+        </form>
+      </section>
     </div>
   );
 };
